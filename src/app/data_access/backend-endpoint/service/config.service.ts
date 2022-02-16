fc370d306f426139253a6e3a7848db21d3d7d0cd
ufokatze1 <bernhard.steirer.4@gmail.com>
2022-02-16 21:29:15 +0100

    add module for backend-endpoints

--- src/app/data_access/backend-endpoint/service/config.service.ts
+++ src/app/data_access/backend-endpoint/service/config.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, of, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { EventQueueService } from 'src/app/feature/gps/service/event-queue.service';
 import {
   GpsTriggerPayload,
   CompassDirection,
 } from 'src/app/feature/gps/util/gps-trigger';
-import { JwtProviderService } from '../authentication/service/jwt-provider.service';
-import { EventIdentifier } from './util/events';
-import { SocketCommunicationMessage } from './util/socket-communication-message';
-import { TriggerType, ManualTriggerPayload } from './util/triggers';
-import { AppEvent, EventDto } from './util/types';
+import { JwtProviderService } from '../../authentication/service/jwt-provider.service';
+import { EventIdentifier } from '../../websocket/util/events';
+import { SocketCommunicationMessage } from '../../websocket/util/socket-communication-message';
+import {
+  TriggerType,
+  ManualTriggerPayload,
+} from '../../websocket/util/triggers';
+import { AppEvent, EventDto } from '../../websocket/util/types';
+import { config } from './dummyData';
 
 @Injectable({
   providedIn: 'root',
@@ -33,8 +37,8 @@ export class ConfigService extends BehaviorSubject<AppEvent[]> {
   }
 
   public requestConfig() {
-    this.http
-      .get(ConfigService.configUrl)
+    let events = config;
+    of(events.events)
       .pipe(
         map((response: any): AppEvent[] =>
           (response.events as EventDto[]).map<AppEvent>((eventDto) => ({
@@ -44,5 +48,19 @@ export class ConfigService extends BehaviorSubject<AppEvent[]> {
         )
       )
       .subscribe(this);
+
+    //  !!! Actual request down below !!!
+
+    /*this.http
+      .get(ConfigService.configUrl)
+      .pipe(
+        map((response: any): AppEvent[] =>
+          (response.events as EventDto[]).map<AppEvent>((eventDto) => ({
+            finish: false,
+            ...eventDto,
+          }))
+        )
+      )
+      .subscribe(this);*/
   }
 }
