90471a861bff457eaa98035bd0f7c5d69bd80fbd
ufokatze1 <bernhard.steirer.4@gmail.com>
2022-02-27 10:09:47 +0100

    add prod envrionment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'jakob-galaxy.at:3000',
+  customerIconPath:
+    'https://cdn.discordapp.com/attachments/545999117214810142/946819673662570557/persona.png',
+  selfPositionIconPath:
+    'https://cdn.discordapp.com/attachments/545999117214810142/947218732244148294/circle-center.png',
 };
