7c114df293f18fcb6bf93e17de72d047276baaaf
ufokatze1 <bernhard.steirer.4@gmail.com>
2022-02-27 11:14:13 +0100

    fix prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'jakob-galaxy.at:3000',
+  apiUrl: 'https://jakob-galaxy.at:3000',
   customerIconPath:
     'https://cdn.discordapp.com/attachments/545999117214810142/946819673662570557/persona.png',
   selfPositionIconPath:
